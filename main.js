d01de6d0a355d96962ead8ff2f8161295e131671
Fuentes Florencia <fuentes.florenciafatimadelosangeles@iesconcepcion.edu.ar>
2025-08-04 20:12:15 -0300

    Codigo actualizado

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-const fetch = require('node-fetch'); 
-const prompt = require('prompt-sync')(); 
+const fetch = require('node-fetch');
+const prompt = require('prompt-sync')();
 
-async function buscarPokemon() {
-    const nombre = prompt('Escribí el nombre del Pokémon: ').toLowerCase();
+const nombre = prompt('Escribí el nombre del Pokémon: ').toLowerCase();
+const url = `https://pokeapi.co/api/v2/pokemon/${nombre}`;
 
-    const url = `https://pokeapi.co/api/v2/pokemon/${nombre}`;
-
-    const respuesta = await fetch(url);
-
+fetch(url)
+  .then(respuesta => {
     if (!respuesta.ok) {
-        console.log('No se encontró el Pokémon. Ingresa nuevamente el nombre.');
-        return;
-      }
-  
-      const datos = await respuesta.json();
-
-      console.log('Resultado:');
-      console.log('Nombre:', datos.name);
-      console.log('Altura:', datos.height);
-      console.log('Tipo(s):', datos.types.map(t => t.type.name).join(', '));
-    } try { 
-    }catch (error) {
-      console.log('Ocurrió un error al buscar el Pokémon.');
+      console.log('❌ No se encontró el Pokémon. Revisá el nombre.');
+      return;
     }
+    return respuesta.json();
+  })
+  .then(datos => {
+    if (!datos) return;
 
-  buscarPokemon();  
+    console.log('✅ Resultado:');
+    console.log('Nombre:', datos.name);
+    console.log('Altura:', datos.height);
+    console.log('Tipo(s):', datos.types.map(t => t.type.name).join(', '));
+  })
+  .catch(error => {
+    console.log('⚠️ Ocurrió un error al buscar el Pokémon.');
+  });
